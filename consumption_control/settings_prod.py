b6ec4cdfd708ba2c7c9c77245ac74c05ffd4dc76
Sergey V <47502482+Prostakkotak@users.noreply.github.com>
2021-01-06 20:16:58 +0300

    Update settings_prod.py

--- consumption_control/settings_prod.py
+++ consumption_control/settings_prod.py
@@ -5,13 +5,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = '3(mudkg)rh8$gjrudijth76w&g)6f_3^5wi#4g_ulaik_rxeeqlo'
 
-DEBUG = True
+DEBUG = False
 
-<<<<<<< HEAD
 ALLOWED_HOSTS = ["consumption-control.std-1388.ist.mospolytech.ru"]
-=======
-ALLOWED_HOSTS = ["127.0.0.1", "std-1388.ist.mospolytech.ru", "172.16.120.10"]
->>>>>>> 0552e4f6c5d0b2ce4fb2bd5c2a2670f55c7a9c2c
 
 DATABASES = {
     'default': {
